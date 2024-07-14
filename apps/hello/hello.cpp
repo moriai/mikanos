ab95625235a9430286464fc0c3952c49f93f6228
Satoshi Moriai <satoshi.moriai@gmail.com>
2024-07-15 00:34:24 +0900

    HelloでSTDIOを使わない

--- apps/hello/hello.cpp
+++ apps/hello/hello.cpp
@@ -1,6 +1,19 @@
+#if USE_STDIO
 #include <cstdio>
 
 int main(int argc, char** argv) {
   printf("Hello, world!\n");
   return 0;
 }
+#else
+#include <stdlib.h>
+#include <unistd.h>
+
+int main() {
+	const char buf[] = "Hello, world!\n";
+	const unsigned int len = sizeof(buf);
+
+	write(1, buf, len);
+	return 0;
+}
+#endif
